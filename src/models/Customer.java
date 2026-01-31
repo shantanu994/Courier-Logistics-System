53300d429f92cdf8272e1fefee6dda36c9ceb3c9
shantanu994 <shantanukulkarni994@gmail.com>
2026-01-31 23:28:32 +0530

    variables created

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -1,5 +1,12 @@
 package models;
 
+import java.sql.Date;
+
 public class Customer {
-    
+    private int customerID;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private Date createdDate;
 }
