05e7d6fa004968f7817ef6534e48d17a4720ffc3
shantanu994 <shantanukulkarni994@gmail.com>
2026-02-09 21:49:25 +0530

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
+import ui.LoginFrame;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        // Start the application by opening Login screen
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new LoginFrame();
+        });
+
+    }
 }
